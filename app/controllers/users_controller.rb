c4245058760b4b806758e1822cef7b727704b975
Joel Grant <joelmgrant@me.com>
2022-03-30 16:52:16 -0600

    Undo usage of subdomains

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,7 @@ class UsersController < ApplicationController
   def create
     user = UsersFacade.create_new_user(user_params.to_h)
     session[:user_id] = user.id
-    # Try to refactor this below:
-    redirect_to dashboard_url controller: 'dashboard', action: 'index'#, controller: 'dashboard', action: 'index')
+    redirect_to "/plantcoach"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :users, only: [:new, :create]
-  constraints subdomain: 'plantcoach' do
-    get '/dashboard', to: 'dashboard#index' #, constraints: {subdomain: 'plantcoach'}
-  end
+  resources :plantcoach, only: [:index]
+  # constraints subdomain: 'plantcoach' do
+  #   get '/dashboard', to: 'dashboard#index' #, constraints: {subdomain: 'plantcoach'}
+  # end
+  # constraints subdomain: 'shop' do
+  #   get '/dashboard', to: 'dashboard#index'
+  # end
 end
