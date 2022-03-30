cd8518629d169227167dbe7bec83aa2b56418d50
Joel Grant <joelmgrant@me.com>
2022-03-30 10:52:43 -0600

    Setup user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-
+  resources :users, only: [:new, :create]
   get '/dashboard', to: 'dashboard#index', constraints: {subdomain: 'plantcoach'}
 end
