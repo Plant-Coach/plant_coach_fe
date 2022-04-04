9cfb7b2fd6fe59679b68a17572be40136e990adb
Joel Grant <joelmgrant@me.com>
2022-04-04 09:56:39 -0600

    Create login routes for logging in a user

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :plantcoach, only: [:index]
   resources :dashboard, only: [:index]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
