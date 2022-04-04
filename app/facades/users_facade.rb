c910e061f8e268db40dd8b1c9ee1daeb9c9a65bd
Joel Grant <joelmgrant@me.com>
2022-04-04 13:49:32 -0600

    Uncomment use of poro

--- app/facades/users_facade.rb
+++ app/facades/users_facade.rb
@@ -2,6 +2,6 @@ class UsersFacade
   def self.create_new_user(user_data)
     result = UsersService.create_new_user(user_data)
     # There needs to be some error handling in case the email address is no good.
-    user_poro = User.new(result[:data][:attributes])
+    # user_poro = User.new(result[:data][:attributes])
   end
 end
