0af508103ede270ba60e35d9ddc99f3b8b614eb6
Joel Grant <joelmgrant@me.com>
2022-04-04 13:48:16 -0600

    Modify test assertions for new formatted response

--- spec/services/users_service_spec.rb
+++ spec/services/users_service_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe UsersService do
         password_confirmation: '12345'
       }
       response = File.read("spec/fixtures/create_user.json")
-  
+      # WebMock.allow_net_connect!
       stub_request(:post, "https://stormy-chamber-46446.herokuapp.com/api/v1/users").
          with(
            body: {"email"=>"joel123456@test.com", "name"=>"Joel Grant", "password"=>"12345", "password_confirmation"=>"12345", "zip_code"=>"80000"},
@@ -22,18 +22,20 @@ RSpec.describe UsersService do
        	  'User-Agent'=>'Faraday v2.2.0'
            }).
          to_return(status: 200, body: response, headers: {})
-
       new_user = UsersService.create_new_user(user)
-
+      # require 'pry'; binding.pry
       expect(new_user).to be_a Hash
-      expect(new_user[:data]).to be_a Hash
-      expect(new_user[:data]).to have_key(:id)
-      expect(new_user[:data]).to have_key(:type)
-      expect(new_user[:data]).to have_key(:attributes)
-      expect(new_user[:data][:attributes]).to have_key(:id)
-      expect(new_user[:data][:attributes]).to have_key(:name)
-      expect(new_user[:data][:attributes]).to have_key(:email)
-      expect(new_user[:data][:attributes]).to have_key(:zip_code)
+      expect(new_user[:user]).to be_a Hash
+      expect(new_user[:user]).to have_key(:data)
+
+      expect(new_user[:user][:data]).to be_a Hash
+      expect(new_user[:user][:data]).to have_key(:id)
+      expect(new_user[:user][:data]).to have_key(:type)
+      expect(new_user[:user][:data]).to have_key(:attributes)
+      expect(new_user[:user][:data][:attributes]).to have_key(:id)
+      expect(new_user[:user][:data][:attributes]).to have_key(:name)
+      expect(new_user[:user][:data][:attributes]).to have_key(:email)
+      expect(new_user[:user][:data][:attributes]).to have_key(:zip_code)
     end
   end
 end
