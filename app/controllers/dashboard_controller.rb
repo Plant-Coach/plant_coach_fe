4a102819b5594f2b92848613854e4c39c997f6ba
Joel Grant <joelmgrant@me.com>
2022-04-04 21:06:00 -0600

    Setup basic dashboard for authenticated users and visitors

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class DashboardController < ApplicationController
   def index
-    
+    if session[:user_id] != nil
+      # Get basic user information
+      # Get a user's plant information
+      # Get a user's frost date information
+    else
+      redirect_to '/', notice: "You must log in!"
+    end
   end
 end
