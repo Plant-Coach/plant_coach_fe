0d2765fb7d45035dc8374ab45d28bbaff9656203
Joel Grant <joelmgrant@me.com>
2022-04-06 14:37:06 -0600

    Modify assertions

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -22,10 +22,26 @@ RSpec.describe 'User Dashboard' do
       click_button "Log In"
 
       expect(current_path).to eq('/dashboard')
-      
+
       expect(page).to have_content("Joel Grant's Dashboard")
       expect(page).to have_content("Name: Joel Grant")
       expect(page).to have_content("Email: joel@plantcoach.com")
+    end
+
+    it 'shows the plants in the users garden' do
+      visit '/'
+      click_button "Log In"
+      expect(current_path).to eq("/login")
+
+      WebMock.allow_net_connect!
+      fill_in :email, with: "joel@plantcoach.com"
+      fill_in :password, with: "12345"
+      click_button "Log In"
+
+      expect(current_path).to eq('/dashboard')
+
+      # require 'pry'; binding.pry
+      expect(page).to have_content("Joel Grant's Plants")
     end
   end
 end
