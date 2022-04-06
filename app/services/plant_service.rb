48ae6ac74b206ef354d7cb0cb281f388c3bd4544
Joel Grant <joelmgrant@me.com>
2022-04-06 14:39:31 -0600

    Create a service for creating plants in the app

--- app/services/plant_service.rb
+++ app/services/plant_service.rb
@@ -7,4 +7,14 @@ class PlantService
     response = conn.get("/api/v1/user_plants")
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.create_plants(plant_details, jwt)
+    conn = Faraday.new(
+      url: "https://stormy-chamber-46446.herokuapp.com",
+      params: plant_details,
+      headers: { Authorization: "Bearer #{jwt}" }
+    )
+    response = conn.post("/api/v1/plants")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
