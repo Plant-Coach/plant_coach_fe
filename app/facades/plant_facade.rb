a9bfbcb9d120d4e556ba693d618a1c8a268c6574
Joel Grant <joelmgrant@me.com>
2022-04-07 20:38:58 -0600

    Create facade moethod for getting user plant data and creating a poro

--- app/facades/plant_facade.rb
+++ app/facades/plant_facade.rb
@@ -5,4 +5,11 @@ class PlantFacade
       Plant.new(plant)
     end
   end
+
+  def self.all_user_plants(jwt)
+    plant_data = PlantService.get_a_users_plants(jwt)
+    plants = plant_data[:data].map do |plant|
+      Plant.new(plant)
+    end
+  end
 end
