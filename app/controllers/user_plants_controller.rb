8f8f71d7501fe34ae6a8fa7d3b1258ac447878c2
Joel Grant <joelmgrant@me.com>
2022-04-08 12:29:11 -0600

    Create a user plant destroy process

--- app/controllers/user_plants_controller.rb
+++ app/controllers/user_plants_controller.rb
@@ -4,4 +4,15 @@ class UserPlantsController < ApplicationController
     flash[:message] = "#{plant.name} #{plant.plant_type} has been added to your garden!"
     redirect_to '/plants'
   end
+
+  def destroy
+    if session[:auth] != nil
+      result = PlantFacade.destroy_user_plant(params[:id], session[:auth])
+      flash[:message] = "Plant removed!"
+      redirect_to "/dashboard"
+    else
+      flash[:message] = "Please log in!"
+      redirect_to "/"
+    end
+  end
 end
