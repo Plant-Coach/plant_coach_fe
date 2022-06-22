5cffcba640f4748c1793284c43d3dce45c0af04e
Joel Grant <joelmgrant@me.com>
2022-06-22 16:31:06 -0600

    Remove colon in bearer token

--- app/services/users_service.rb
+++ app/services/users_service.rb
@@ -9,7 +9,7 @@ class UsersService
 
   def self.user_data(jwt)
     conn = Faraday.new(url: "https://stormy-chamber-46446.herokuapp.com/",
-      headers: { Authorization: "Bearer: #{jwt}" }
+      headers: { Authorization: "Bearer #{jwt}" }
     )
     response = conn.get("/api/v1/users")
     JSON.parse(response.body, symbolize_names: true)
@@ -17,7 +17,7 @@ class UsersService
 
   def self.delete_user(id, jwt)
     conn = Faraday.new(url: "https://stormy-chamber-46446.herokuapp.com",
-      headers: { Authorization: "Bearer: #{jwt}" }
+      headers: { Authorization: "Bearer #{jwt}" }
     )
     conn.delete("/api/v1/users/#{id}")
   end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,10 +1,9 @@
 class WeatherService
   def self.get_weather(jwt)
-    conn = Faraday.new(url: "https://stormy-chamber-46446.herokuapp.com/",
-      headers: { Authorization: "Bearer: #{jwt}" }
+    conn = Faraday.new(url: "https://stormy-chamber-46446.herokuapp.com",
+      headers: { Authorization: "Bearer #{jwt}" }
     )
     response = conn.get("/api/v1/forecast")
-
     result = JSON.parse(response.body, symbolize_names: true)
   end
 end
