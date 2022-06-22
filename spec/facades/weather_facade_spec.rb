4751a275e0161ed251a39594bae69f21679add96
Joel Grant <joelmgrant@me.com>
2022-06-22 17:02:50 -0600

    Modify stub for weather test

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -1,21 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe WeatherFacade do
+  before(:each) do
+    WebMock.allow_net_connect!
+    login_params = { email: 'joel@plantcoach.com', password: '12345' }
+    @authenticated_user = SessionService.user_login(login_params)
+  end
   describe '::get_forecast' do
-    xit 'returns the forecast data for the location' do
-      zip_code = 80122
+    it 'returns the forecast data for the location' do
       response = File.read("spec/fixtures/get_forecast.json")
-      # stub_request(:post, "https://stormy-chamber-46446.herokuapp.com/api/v1/forecast").
-      #    with(
-      #      body: {"location"=>"80122"},
-      #      headers: {
-      #  	  'Accept'=>'*/*',
-      #  	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      #  	  'Content-Type'=>'application/x-www-form-urlencoded',
-      #  	  'User-Agent'=>'Faraday v2.2.0'
-      #      }).
-      #    to_return(status: 200, body: response, headers: {})
-      data = WeatherFacade.get_forecast(80122)
+      stub_request(:post, "https://stormy-chamber-46446.herokuapp.com/api/v1/forecast").
+         with(
+           body: {"location"=>"80122"},
+           headers: {
+       	  'Accept'=>'*/*',
+       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	  'Content-Type'=>'application/x-www-form-urlencoded',
+       	  'User-Agent'=>'Faraday v2.2.0'
+           }).
+         to_return(status: 200, body: response, headers: {})
+      data = WeatherFacade.get_forecast(@authenticated_user[:jwt])
       expect(data).to be_an Array
       data.each do |forecast|
         expect(forecast).to be_a Forecast
